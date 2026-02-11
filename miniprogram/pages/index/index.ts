ce1b99ae569f577ed9d108ca9a5454a9352422e1
ZenBook Dev <122558187@qq.com>
2026-02-11 19:44:49 +0800

    feat: add input modal

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,9 +1,38 @@
 // index.ts
 Page({
     data: {
-
+        showInput: false,
+        inputValue: ''
     },
     onLoad() {
 
     },
+
+    onTapAdd() {
+        this.setData({
+            showInput: true
+        });
+    },
+
+    onHideInput() {
+        this.setData({
+            showInput: false,
+            inputValue: '' // Reset input
+        });
+    },
+
+    onInputConfirm(e: any) {
+        const value = e.detail.value;
+        console.log("User Input:", value);
+
+        if (value && value.trim()) {
+            wx.showToast({
+                title: '记下来了!',
+                icon: 'success'
+            });
+            // TODO: Parse and save transaction
+        }
+
+        this.onHideInput();
+    }
 })
